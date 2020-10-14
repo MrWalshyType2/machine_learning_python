e395d240fb13df319c467ac2171ac4761d216196
MrWalshyType2 <mwalsh@academytrainee.com>
2020-10-14 10:29:50 +0100

    Added example on variance

--- standard_deviation.py
+++ standard_deviation.py
@@ -1,5 +1,6 @@
 import numpy
 # The standard deviation is the amount of spread between values.
+#   - Represented by the Sigma symbol
 
 #   - Low standard deviation = most numbers are close to the mean (average) value
 #   - High standard deviation = values are spread out over a wider range
@@ -10,5 +11,22 @@ speed2 = [32,111,138,28,59,77,97]
 lowDeviation = numpy.std(speed)
 highDeviation = numpy.std(speed2)
 
+print("DATASET SPEED: ", speed)
 print("LOW DEVIATION: ", lowDeviation)
+
+print("\nDATASET SPEED2: ", speed2)
 print("HIGH DEVIATION: ", highDeviation)
+
+###############################################################################
+# Variance is another number that indicates how spread out values are.
+#   - Represented by the Sigma Square
+#   - The square root of the variance is the standard deviation.
+#   - The square of the standard deviation is the variance.
+
+# STEPS:
+#   - Find the mean of a data set
+#   - Find the difference from the mean for each value
+#   - Find the square value for each difference
+#   - The variance is the average/mean of these squared differences.
+highDeviationVariance = numpy.var(speed2)
+print("HIGH VARIANCE: ", highDeviationVariance)
