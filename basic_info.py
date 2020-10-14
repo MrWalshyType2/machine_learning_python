458412d7b3f01faa70daafa4bdf08599c8baddfb
MrWalshyType2 <mwalsh@academytrainee.com>
2020-10-14 10:08:38 +0100

    Added basic info on data sets and data types in ml

---
+++ basic_info.py
@@ -0,0 +1,16 @@
+# Machine learning concerns making a computer learn from data and statistics.
+#   - A machine learning program can be said to analyse data to learn to predict outcomes.
+
+# A Data Set is any collection of data in a computer.
+myList = [33, 24, 24, 21, 54, 33]
+
+# The data in a data set has to be analysed to predict outcomes, so it important to know what
+# data type is being dealt with.
+
+# Numerical data (numbers)
+#   - Discrete Data (numbers limited to integers)
+#   - Continuous Data (numbers of an infinite value)
+
+# Categorical data (values that cannot be measured against each other, such as colour values and yes/no values).
+
+# Ordinal data (similar to categorical data, but can be measured against each other, such as comparing grades).
